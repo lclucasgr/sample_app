bfb82bacaca5fd291dd1be9ce420f3ffcf7e1ab5
Lucas <lclucasgr@gmail.com>
2018-05-25 00:23:04 -0300

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
@@ -33,10 +33,15 @@ class User < ApplicationRecord
        UserMailer.account_activation(self).deliver_now
     end
 
-     def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
+    def create_reset_digest
+       self.reset_token = User.new_token
+       update_columns(reset_digest:  User.digest(reset_token), reset_sent_at: Time.zone.now)
     end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
 
     def remember
       self.remember_token = User.new_token
@@ -53,6 +58,10 @@ class User < ApplicationRecord
       update_attribute(:remember_digest, nil)
     end
 
+    def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   private
 
     # Converts email to all lower-case.
@@ -60,4 +69,10 @@ class User < ApplicationRecord
       #self.email = email.downcase
       email.downcase!
     end
+
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  #get 'password_resets/new'
+
+  #get 'password_resets/edit'
+
+  #get 'sessions/new'
 
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
@@ -10,7 +14,8 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :users
+  resources :users #cria todas as rotas necessárias para o crud do usuario :users
   resources :account_activations, only: [:edit]
-  #cria todas as rotas necessárias para o crud do usuario :users
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  
 end
