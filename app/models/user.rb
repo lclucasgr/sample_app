2ad3fc31f053adff572ca63223b8b869cfafa84b
Lucas <lclucasgr@gmail.com>
2018-05-30 00:32:26 -0300

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -80,7 +80,7 @@ class User < ApplicationRecord
                      OR user_id = :user_id", user_id: id)
   end
 
-   def follow(other_user)
+  def follow(other_user)
     following << other_user
   end
 
